e6684f811f9baa4e099e0806bc645b2742f1cc69
Ryan Manchester <ryanm17@gmail.com>
2021-11-05 16:09:49 -0400

    implements signing in with Google auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 import 'firebase/auth';
 import { initializeApp } from "firebase/app";
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -26,27 +26,41 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    const user = result.user;
-  }).catch(error => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    const email = error.email;
-    const credential = GoogleAuthProvider.credentialFromError(error);
-  })
-// const firestore = firebase.firestore();
+
+
+ const firestore = getFirestore();
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
       <header className="App-header">
       <h1>Welcome to Superchat</h1>
       </header>
+      <section>
+      {user ? `Hello! Thank you for signing in!` : <SignIn /> }
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+      }).catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      })
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign In With Google</button>
+  )
 }
 
 export default App;
